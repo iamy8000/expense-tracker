6876b61e21e64809e012ad0605686d814a794b06
iamy8000 <amy8000@gmail.com>
2020-11-11 01:18:59 +0800

    refactor: modify express routers

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+
 const Record = require('./models/record')
+const router = require('./routes')
 
 mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -21,59 +23,11 @@ app.set('view engine', 'hbs')
 
 app.use(methodOverride('_method'))
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(router)
 
-app.get('/', (req, res) => {
-  Record.find()
-    .lean()
-    .sort({ date: 'desc' })
-    .then(records => {
-      res.render('index', { records })
-      console.log(records.date)
-    })
-    .catch(error => console.error(error))
-})
 
-app.get('/records/new', (req, res) => {
-  return res.render('new')
-})
 
-app.post('/records', (req, res) => {
-  const { name, category, date, amount } = req.body
-  return Record.create({ name, category, date, amount })
-    .then(() => { res.redirect('/') })
-    .catch(error => console.log(error))
-})
 
-app.get('/records/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Record.findById(id)
-    .lean()
-    .then((record) => res.render('edit', { record }))
-    .catch(error => console.log(error))
-})
-
-app.put('/records/:id', (req, res) => {
-  const id = req.params.id
-  const { name, category, date, amount } = req.body
-  return Record.findById(id)
-    .then(record => {
-      record.name = name;
-      record.category = category;
-      record.date = date;
-      record.amount = amount;
-      return record.save()
-    })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
-
-app.delete('/records/:id', (req, res) => {
-  const id = req.params.id
-  return Record.findById(id)
-    .then(record => record.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
 
 app.listen(3000, () => {
   console.log('App is running on http://localhost: 3000')
---
+++ routes/modules/home.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+const Record = require('../../models/record')
+
+router.get('/', (req, res) => {
+  Record.find()
+    .lean()
+    .sort({ date: 'desc' })
+    .then(records => {
+      res.render('index', { records })
+    })
+    .catch(error => console.error(error))
+})
+
+module.exports = router
