d1ac17ad5b9b536fd1c4d320ff39099802394b42
iamy8000 <amy8000@gmail.com>
2020-11-10 17:43:52 +0800

    feat: add create record function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const Record = require('./models/record')
 
 mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -17,12 +18,24 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   Record.find()
     .lean()
     .then(records => res.render('index', { records }))
     .catch(error => console.error(error))
+})
+
+app.get('/records/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/records', (req, res) => {
+  const { name, category, date, amount } = req.body
+  return Record.create({ name, category, date, amount })
+    .then(() => { res.redirect('/') })
+    .catch(error => console.log(error))
 })
 
 app.listen(3000, () => {
