d40fe6fdb3159dacf6614fa0f436be9841febcbe
iamy8000 <amy8000@gmail.com>
2020-11-11 01:34:41 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,33 +1,19 @@
 const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
-const Record = require('./models/record')
 const router = require('./routes')
+require('./config/mongoose')
 
-mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
-
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+const app = express()
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-
 app.use(methodOverride('_method'))
 app.use(bodyParser.urlencoded({ extended: true }))
+
 app.use(router)
-
-
-
-
 
 app.listen(3000, () => {
   console.log('App is running on http://localhost: 3000')
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,14 +1,7 @@
-const mongoose = require('mongoose')
 const Record = require('../record')
+const db = require('../../config/mongoose')
 
-mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error')
-})
 db.once('open', () => {
-  console.log('mongodb connect!')
   for (let i = 0; i < 5; i++) {
     Record.create({
       name: 'name' + i,
@@ -17,4 +10,5 @@ db.once('open', () => {
       amount: i
     })
   }
+  console.log('recordSeeder done')
 })
