d6ee6b4f5cf772cd9eddf76b27652142e714faeb
iamy8000 <amy8000@gmail.com>
2020-11-11 16:41:31 +0800

    feat: add totalAmount function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,14 +3,29 @@ const router = express.Router()
 
 const Record = require('../../models/record')
 
+let totalAmount = 0
+
 router.get('/', (req, res) => {
   Record.find()
     .lean()
     .sort({ date: 'desc' })
     .then(records => {
-      res.render('index', { records })
+      records.forEach(record => {
+        totalAmount = totalAmount + record.amount
+      })
+      res.render('index', { records, totalAmount })
     })
     .catch(error => console.error(error))
 })
+
+// router.get('/', (req, res) => {
+//   Record.find()
+//     .lean()
+//     .sort({ date: 'desc' })
+//     .then(records => {
+//       res.render('index', { records })
+//     })
+//     .catch(error => console.error(error))
+// })
 
 module.exports = router
